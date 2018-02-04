168f1e6a3f4467437e7739f81d6c9ee7523ebd48
Ganapathi004 <ganapathibasamsetti@gmail.com>
2018-02-04 18:02:17 +0530

    Version Alpha

---
+++ README.md
@@ -0,0 +1,10 @@
+# Style It
+
+**Adds little text styles for websites currently for Spring Framework Documentation Website. The styles apply for any page under the domain `docs.spring.io`**
+
+## What it does
+
+This extension just includes:
+
+* a content script, "styleit.js", that is injected into any pages
+under "docs.spring.io/"
---
+++ js/styleit.js
@@ -0,0 +1,7 @@
+/*
+ Adds little text styles for Spring Framework Documentation Website.
+*/
+var styleTag = document.createElement("link");
+var styles = document.createTextNode('#header { padding: 0 0.5em 0 0.5em; } #tocbot > ul.toc-list { margin-bottom:0; font-family:Georgia; } #toc li { font-family:Georgia; } .paragraph p { text-align: justify; text-justify: auto; hyphens: auto; } .icon { display: none; }');
+styleTag.appendChild(styles);
+document.body.appendChild(styleTag);
